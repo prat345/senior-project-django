47c838a4f694a130bbee6ac83d8f5cea8282098f
prat345 <pratprasert@gmail.com>
2023-11-26 19:43:45 +0700

    update envvar

--- demoapp/mongo.py
+++ demoapp/mongo.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / ".env")
+# load_dotenv(BASE_DIR / ".env")
 MONGO_URI = os.getenv("MONGO_URI") # Atlas
 # print(MONGO_URI)
 # MONGO_URI = 'mongodb://localhost:27017' # Compass 
