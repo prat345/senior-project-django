b202964cf42a02110f2081046ab41e15c27b9e5a
prat345 <pratprasert@gmail.com>
2023-11-26 20:20:44 +0700

    final v

--- demoapp/urls.py
+++ demoapp/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',views.index, name="home"),
+    path('',views.summary, name="home"),
     path('about',views.about, name="about"),
     path('summary',views.summary, name="summary"),
     path('testdrive',views.testdrive),
--- senior_project/settings.py
+++ senior_project/settings.py
@@ -31,8 +31,8 @@ PASSWORD = os.getenv('PASSWORD')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = bool(os.environ.get("DEBUG"))
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG"))
+# DEBUG = True
 
 ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS")]
 
